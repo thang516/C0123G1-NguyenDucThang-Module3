dfbd0c2ec451b871e88d8a27a3f3a6e395f7f8d4
thang516 <123801341+thang516@users.noreply.github.com>
2023-04-11 13:33:49 +0700

    Create bai1.sql

---
+++ bai1/bai1.sql
@@ -0,0 +1,12 @@
+create database student_management;
+use student_management;
+create table Class(
+id int,
+`name` varchar(50)
+);
+create table Teacher(
+id int,
+`name` varchar(50),
+age int,
+country varchar(50)
+);
