fa53116b708c154c5b22506bc4e5a32849ad8768
thang516 <123801341+thang516@users.noreply.github.com>
2023-04-12 09:28:17 +0700

    Update bai1.sql

--- bai1/bai1.sql
+++ bai1/bai1.sql
@@ -1,12 +1,13 @@
-create database student_management;
-use student_management;
-create table Class(
-id int,
-`name` varchar(50)
+CREATE DATABASE student_management;
+USE student_management;
+CREATE TABLE Classes(
+id INT,
+`name` VARCHAR(50)
 );
-create table Teacher(
-id int,
-`name` varchar(50),
-age int,
-country varchar(50)
+CREATE TABLE Teacher(
+id INT,
+`name` VARCHAR(50),
+age INT,
+country VARCHAR(50)
 );
+
