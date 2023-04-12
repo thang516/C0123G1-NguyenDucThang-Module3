d80dd0346a71966f4a3e321e7037a8f5f61b623d
thang516 <123801341+thang516@users.noreply.github.com>
2023-04-12 15:40:54 +0700

    baitapss2

---
+++ bai_tap/ss2_bai_tap/bai1.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE vat_tu;
+USE vat_tu;
+CREATE TABLE so_dien_thoai(
+ten_so_dien_thoai VARCHAR(11) PRIMARY KEY 
+
+);
+CREATE TABLE phieu_xuat(
+so_px INT PRIMARY KEY,
+ngay_xuat DATE
+);
+CREATE TABLE phieu_nhap(
+so_pn INT PRIMARY KEY,
+ngay_nhap DATE
+);
+CREATE TABLE nha_cung_cap (
+    ma_nha_cung_cap INT PRIMARY KEY,
+    ten_nha_cung_cap VARCHAR(50),
+    dia_chi VARCHAR(50),
+    ten_so_dien_thoai VARCHAR(11),
+    FOREIGN KEY (ten_so_dien_thoai) REFERENCES so_dien_thoai (ten_so_dien_thoai)
+);
+CREATE TABLE don_dat_hang(
+so_dat_hang INT PRIMARY KEY ,
+ngay_dat_hang DATE,
+ma_nha_cung_cap INT, FOREIGN KEY (ma_nha_cung_cap)REFERENCES nha_cung_cap(ma_nha_cung_cap)
+);
+CREATE TABLE vat_tu (
+    ma_vat_tu INT PRIMARY KEY,
+    ten_vat_tu VARCHAR(50)
+);
+CREATE TABLE chi_tiet_phieu_xuat (
+don_gia_xuat DOUBLE,
+so_luong_xuat INT,
+ so_px INT,
+    FOREIGN KEY (so_px)
+        REFERENCES phieu_xuat (so_px),
+         so_pn INT,
+   ma_vat_tu INT,FOREIGN KEY (ma_vat_tu) REFERENCES vat_tu(ma_vat_tu)
+);
+CREATE TABLE chi_tiet_phieu_nhap (
+    don_gia_nhap DOUBLE,
+    so_luong_nhap INT,
+    so_pn INT,
+    FOREIGN KEY (so_pn)
+        REFERENCES phieu_nhap (so_pn),
+    ma_vat_tu INT,
+    FOREIGN KEY (ma_vat_tu)
+        REFERENCES vat_tu (ma_vat_tu)
+);
+CREATE TABLE chi_tiet_don_hang (
+ma_vat_tu INT,FOREIGN KEY (ma_vat_tu) REFERENCES vat_tu(ma_vat_tu),
+so_dat_hang INT, FOREIGN KEY (so_dat_hang) REFERENCES don_dat_hang(so_dat_hang)
+);
+
---
+++ bai_tap/ss2_bai_tap/bai2.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE quan_li_ban_hang;
+USE quan_li_ban_hang;
+
+CREATE TABLE customer(
+customer_id INT PRIMARY KEY AUTO_INCREMENT ,
+customer_name VARCHAR(50),
+customer_age BIT
+
+);
+CREATE TABLE `order` (
+order_id INT PRIMARY KEY,
+customer_id INT, FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+order_date DATE ,
+order_total_price DOUBLE
+);
+CREATE TABLE product(
+product_id INT PRIMARY KEY,
+product_name VARCHAR(50),
+product_price DOUBLE
+);
+CREATE TABLE order_detail(
+order_id INT ,FOREIGN KEY(order_id)REFERENCES `order`(order_id),
+product_id INT ,FOREIGN KEY(product_id)REFERENCES product(product_id),
+order_detail_qtl INT
+);
